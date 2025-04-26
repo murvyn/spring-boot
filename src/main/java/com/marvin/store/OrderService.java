e5609fd71ad74ea0bb5e33c1003bd89e6c05055e
Murvyn <marvin.asamoah.123@gmail.com>
2025-04-26 01:31:44 +0000

    Add OrderService and payment service implementations (PayPal, Stripe) with order placement functionality

---
+++ src/main/java/com/marvin/store/OrderService.java
@@ -0,0 +1,16 @@
+package com.marvin.store;
+
+public class OrderService {
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+    public void placeOrder() {
+        paymentService.processPayment(10);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+}
---
+++ src/main/java/com/marvin/store/PaymentService.java
@@ -0,0 +1,7 @@
+package com.marvin.store;
+
+public interface PaymentService {
+
+    void processPayment(double amount);
+
+}
